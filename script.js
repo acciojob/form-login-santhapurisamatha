d4c163988cb821509922b981ca4e05b6e2fc4d0c
Samatha Santhapuri <santhapurisamatha@gmail.com>
2026-09-02 13:16:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
-
+    event.preventDefault();
+    
+    const form = document.getElementById("form1");
+    const firstName = form.elements["fname"].value;
+    const lastName = form.elements["lname"].value;
+    
+    alert(`${firstName} ${lastName}`);
 }
